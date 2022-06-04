4811ef8e96db27a8f0de0478d641a2482a89a7bc
johannesschobel <johannes.schobel@googlemail.com>
2022-06-04 10:08:19 +0200

    chore: remove old auth routes

--- apps/api/src/app/modules/api.module.ts
+++ apps/api/src/app/modules/api.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ApiAuthFeatureModule } from '@trackyourhealth/api/auth/feature';
 import { ApiHealthFeatureModule } from '@trackyourhealth/api/health/feature';
 import { ApiStudiesModule } from '@trackyourhealth/api/studies';
 
 @Module({
-  imports: [ApiAuthFeatureModule, ApiHealthFeatureModule, ApiStudiesModule],
+  imports: [ApiHealthFeatureModule, ApiStudiesModule],
 })
 export class ApiModule {}
