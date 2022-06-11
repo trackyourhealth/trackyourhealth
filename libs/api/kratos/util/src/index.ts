f0cd3980c565d87a083517acfc85ac3e53caebfb
johannesschobel <johannes.schobel@googlemail.com>
2022-06-11 21:32:14 +0200

    chore: use own session model

--- libs/api/kratos/util/src/index.ts
+++ libs/api/kratos/util/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/api-kratos-util.module';
 export * from './lib/decorators/kratos-user.decorator';
 export * from './lib/guards/kratos.guard';
+export * from './lib/models/kratos.session';
