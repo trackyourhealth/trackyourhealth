2941d40822da7d0772d638a3626cd3319f73306f
johannesschobel <johannes.schobel@googlemail.com>
2022-06-11 22:18:31 +0200

    chore: get rid of auth libs

--- libs/api/auth/data/src/lib/models/access-token.model.ts
+++
@@ -1,8 +0,0 @@
-import { CoreModel } from '@trackyourhealth/api/core/util';
-
-export interface AccessTokenModel extends CoreModel {
-  accessToken: string;
-  tokenType: string;
-  refreshToken: string;
-  expiresIn: number;
-}
--- libs/api/auth/data/src/lib/models/credentials.model.ts
+++
@@ -1,6 +0,0 @@
-import { CoreModel } from '@trackyourhealth/api/core/util';
-
-export interface CredentialsModel extends CoreModel {
-  email: string;
-  password: string;
-}
