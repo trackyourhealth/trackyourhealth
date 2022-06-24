bb57d0ffd3092bc9ef488ce1ec552e0bed445ea5
patricksteiger <patricksteiger7@gmail.com>
2022-06-24 14:12:41 +0200

    feat(api): add getStudyById with tests

--- libs/api/study/feature/src/lib/api-study-feature.controller.ts
+++ libs/api/study/feature/src/lib/api-study-feature.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+} from '@nestjs/common';
+import { Study } from '@prisma/client';
 import { ApiStudyDataService } from '@trackyourhealth/api/study/data';
 
 @Controller('api-study-feature')
@@ -6,7 +13,16 @@ export class ApiStudyFeatureController {
   constructor(private readonly apiStudyDataService: ApiStudyDataService) {}
 
   @Get()
-  async getAllActiveStudies() {
-    return await this.apiStudyDataService.getAllActiveStudies();
+  async getAllActiveStudies(): Promise<Study[]> {
+    return this.apiStudyDataService.getAllActiveStudies();
+  }
+
+  @Get(':studyId')
+  async getStudyById(@Param('studyId') studyId: string): Promise<Study> {
+    const result = await this.apiStudyDataService.getStudyById(studyId);
+    if (result == null) {
+      throw new HttpException('No such study exists', HttpStatus.NOT_FOUND);
+    }
+    return result;
   }
 }
