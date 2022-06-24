9773aaba42e160919972ae4b7be5cba2c61b2531
patricksteiger <patricksteiger7@gmail.com>
2022-06-24 11:51:11 +0200

    feat(api): add getAllActiveStudies with tests

--- libs/api/study/data/src/index.ts
+++ libs/api/study/data/src/index.ts
@@ -1,1 +1,3 @@
 export * from './lib/api-study-data';
+export * from './lib/api-study-data.module';
+export * from './lib/services/api-study-data.service';
--- libs/api/study/feature/src/lib/api-study-feature.controller.ts
+++ libs/api/study/feature/src/lib/api-study-feature.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ApiStudyDataService } from '@trackyourhealth/api/study/data';
 
 @Controller('api-study-feature')
-export class ApiStudyFeatureController {}
+export class ApiStudyFeatureController {
+  constructor(private readonly apiStudyDataService: ApiStudyDataService) {}
+
+  @Get()
+  async getAllActiveStudies() {
+    return await this.apiStudyDataService.getAllActiveStudies();
+  }
+}
