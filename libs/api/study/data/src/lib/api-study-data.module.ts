4c608939c1b9fbfc72e4ed101e4345054c811b32
patricksteiger <patricksteiger7@gmail.com>
2022-06-28 12:26:40 +0200

    chore(api): add provider to ApiStudyDataModule

--- libs/api/study/data/src/lib/api-study-data.module.ts
+++ libs/api/study/data/src/lib/api-study-data.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from '@prisma-utils/nestjs-prisma';
 
 import { ApiStudyDataService } from './services/api-study-data.service';
 
 @Module({
   controllers: [],
-  providers: [ApiStudyDataService],
+  providers: [ApiStudyDataService, PrismaService],
   exports: [ApiStudyDataService],
 })
 export class ApiStudyDataModule {}
