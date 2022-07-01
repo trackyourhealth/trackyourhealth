1f641350ce87d8ff77d3885cbecea7b78651be67
patricksteiger <patricksteiger7@gmail.com>
2022-07-01 15:03:17 +0200

    feat(testing): add testing lib with first mock

---
+++ libs/api/testing/util/src/lib/api-testing-util.spec.ts
@@ -0,0 +1,7 @@
+import { apiTestingUtil } from './api-testing-util';
+
+describe('apiTestingUtil', () => {
+  it('should work', () => {
+    expect(apiTestingUtil()).toEqual('api-testing-util');
+  });
+});
---
+++ libs/api/testing/util/src/lib/mocks/study.prisma.mock.ts
@@ -0,0 +1,67 @@
+import { Prisma, Study } from '@prisma/client';
+
+const studies: Study[] = [
+  {
+    id: '4321',
+    name: 'teststudy4321',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    title: 'teststudy',
+    description: 'study for testing',
+    isActive: false,
+    startsAt: new Date(),
+    endsAt: new Date(),
+  },
+  {
+    id: '1234',
+    name: 'teststudy1234',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    title: 'teststudy',
+    description: 'study for testing',
+    isActive: true,
+    startsAt: new Date(),
+    endsAt: new Date(),
+  },
+];
+
+const mockFindManyStudies = async (
+  options?: Prisma.StudyFindManyArgs,
+): Promise<Study[]> => {
+  const whereOpts = options?.where;
+  if (whereOpts) {
+    if (typeof whereOpts.isActive === 'boolean') {
+      return studies.filter((study) => study.isActive === whereOpts.isActive);
+    }
+  }
+  return studies;
+};
+
+const mockFindUniqueStudy = async (
+  options?: Prisma.StudyFindUniqueArgs,
+): Promise<Study | null> => {
+  const whereOpts = options?.where;
+  if (whereOpts) {
+    if (whereOpts.id != null) {
+      const uniqueStudy = studies.filter((study) => study.id === whereOpts.id);
+      if (uniqueStudy.length === 0) return null;
+      if (uniqueStudy.length === 1) return uniqueStudy[0];
+      throw new Error('Multiple elements are not possible for unique values');
+    }
+  }
+  return null;
+};
+
+const mockStudyPrisma = {
+  study: {
+    findMany: mockFindManyStudies,
+    findUnique: mockFindUniqueStudy,
+  },
+};
+
+export const prismaStudyMock = {
+  service: mockStudyPrisma,
+  getAllStudies: (): Study[] => [...studies],
+  getActiveStudies: (): Study[] => studies.filter((study) => study.isActive),
+  getFirstStudy: (): Study => studies[0],
+};
