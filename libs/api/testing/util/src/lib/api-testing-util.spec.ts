66987a995e7bd2165bbd2f6da7873f2586ebf4b9
patricksteiger <patricksteiger7@gmail.com>
2022-07-06 14:12:41 +0200

    chore(testing): delete needless test

--- libs/api/testing/util/src/lib/api-testing-util.spec.ts
+++
@@ -1,7 +0,0 @@
-import { apiTestingUtil } from './api-testing-util';
-
-describe('apiTestingUtil', () => {
-  it('should work', () => {
-    expect(apiTestingUtil()).toEqual('api-testing-util');
-  });
-});
