285c36255ae69b7c7f3b4f29fa243aea2940c1a9
patricksteiger <patricksteiger7@gmail.com>
2022-07-18 14:33:03 +0200

    test(api): add tests for create, update, delete

--- libs/api/study/feature/src/lib/api-study-feature.controller.spec.ts
+++ libs/api/study/feature/src/lib/api-study-feature.controller.spec.ts
@@ -1,4 +1,5 @@
 import { Test } from '@nestjs/testing';
+import { Prisma } from '@prisma/client';
 import { PrismaService } from '@prisma-utils/nestjs-prisma';
 import { ParsedQueryModel } from '@trackyourhealth/api/common/util';
 import { ApiStudyDataService } from '@trackyourhealth/api/study/data';
@@ -6,10 +7,21 @@ import { prismaStudyMock } from '@trackyourhealth/api/testing/util';
 
 import { ApiStudyFeatureController } from './api-study-feature.controller';
 
+const mockCreate = jest.fn();
+const mockDelete = jest.fn();
+const mockUpdate = jest.fn();
+
 describe('ApiStudyFeatureController', () => {
   let controller: ApiStudyFeatureController;
 
   beforeEach(async () => {
+    prismaStudyMock.service.study.create = mockCreate;
+    prismaStudyMock.service.study.delete = mockDelete;
+    prismaStudyMock.service.study.update = mockUpdate;
+    mockCreate.mockClear();
+    mockDelete.mockClear();
+    mockUpdate.mockClear();
+
     const module = await Test.createTestingModule({
       providers: [ApiStudyDataService, PrismaService],
       controllers: [ApiStudyFeatureController],
@@ -25,7 +37,7 @@ describe('ApiStudyFeatureController', () => {
     expect(controller).toBeTruthy();
   });
 
-  describe('getAllActiveStudies', () => {
+  describe('getAllStudies', () => {
     it('returns all active studies', async () => {
       expect.assertions(1);
       const result = await controller.getAllStudies({} as ParsedQueryModel);
@@ -48,6 +60,97 @@ describe('ApiStudyFeatureController', () => {
       return controller
         .getStudyById(invalidStudyId)
         .catch((e) => expect(e.name).toStrictEqual('HttpException'));
+    });
+  });
+
+  describe('createStudy', () => {
+    it('returns valid study', async () => {
+      const expectedStudy = prismaStudyMock.getFirstStudy();
+      mockCreate.mockResolvedValueOnce(expectedStudy);
+      expect.assertions(3);
+      const input: Prisma.StudyCreateInput = {
+        name: 'a study',
+      };
+      const result = await controller.createStudy(input);
+      expect(result).toStrictEqual(expectedStudy);
+      expect(mockCreate).toBeCalledTimes(1);
+      expect(mockCreate).toHaveBeenCalledWith({ data: input });
+    });
+
+    it('throws HttpException on invalid studyId', async () => {
+      mockCreate.mockRejectedValueOnce(new Error());
+      expect.assertions(4);
+      const input: Prisma.StudyCreateInput = {
+        name: 'invalid id',
+      };
+      await controller.createStudy(input).catch((e) => {
+        expect(e.status).toStrictEqual(404);
+        expect(e.name).toStrictEqual('HttpException');
+      });
+      expect(mockCreate).toBeCalledTimes(1);
+      expect(mockCreate).toHaveBeenCalledWith({ data: input });
+    });
+  });
+
+  describe('deleteStudy', () => {
+    it('returns valid study', async () => {
+      const expectedStudy = prismaStudyMock.getFirstStudy();
+      mockDelete.mockResolvedValueOnce(expectedStudy);
+      expect.assertions(3);
+      const studyId = expectedStudy.id;
+      const result = await controller.deleteStudy(studyId);
+      expect(result).toStrictEqual(expectedStudy);
+      expect(mockDelete).toBeCalledTimes(1);
+      expect(mockDelete).toHaveBeenCalledWith({ where: { id: studyId } });
+    });
+
+    it('throws HttpException on invalid studyId', async () => {
+      mockDelete.mockRejectedValueOnce(new Error());
+      expect.assertions(4);
+      const studyId = 'invalid id';
+      await controller.deleteStudy(studyId).catch((e) => {
+        expect(e.status).toStrictEqual(404);
+        expect(e.name).toStrictEqual('HttpException');
+      });
+      expect(mockDelete).toBeCalledTimes(1);
+      expect(mockDelete).toHaveBeenCalledWith({ where: { id: studyId } });
+    });
+  });
+
+  describe('updateStudy', () => {
+    it('returns valid study', async () => {
+      const expectedStudy = prismaStudyMock.getFirstStudy();
+      mockUpdate.mockResolvedValueOnce(expectedStudy);
+      expect.assertions(3);
+      const studyId = expectedStudy.id;
+      const input: Prisma.StudyUpdateInput = {
+        name: 'new name',
+      };
+      const result = await controller.updateStudy(studyId, input);
+      expect(result).toStrictEqual(expectedStudy);
+      expect(mockUpdate).toBeCalledTimes(1);
+      expect(mockUpdate).toHaveBeenCalledWith({
+        data: input,
+        where: { id: studyId },
+      });
+    });
+
+    it('throws HttpException on invalid studyId', async () => {
+      mockUpdate.mockRejectedValueOnce(new Error());
+      expect.assertions(4);
+      const studyId = 'invalid id';
+      const input: Prisma.StudyUpdateInput = {
+        name: 'unused name',
+      };
+      await controller.updateStudy(studyId, input).catch((e) => {
+        expect(e.status).toStrictEqual(404);
+        expect(e.name).toStrictEqual('HttpException');
+      });
+      expect(mockUpdate).toBeCalledTimes(1);
+      expect(mockUpdate).toHaveBeenCalledWith({
+        data: input,
+        where: { id: studyId },
+      });
     });
   });
 });
