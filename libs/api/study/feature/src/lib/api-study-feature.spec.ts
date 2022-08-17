c5009928dee6d1ace91d8b47c272f0fad396eb7b
patricksteiger <patricksteiger7@gmail.com>
2022-08-17 14:41:38 +0200

    test(api): add more e2e tests and refactor tests

--- libs/api/study/feature/src/lib/api-study-feature.spec.ts
+++ libs/api/study/feature/src/lib/api-study-feature.spec.ts
@@ -2,7 +2,14 @@ import { HttpStatus, INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { PrismaService } from '@prisma-utils/nestjs-prisma';
 import { ApiStudyDataModule } from '@trackyourhealth/api/study/data';
-import { prismaStudyMock } from '@trackyourhealth/api/testing/util';
+import {
+  dbConnectionError,
+  dbKnownError,
+  defaultQueryValues,
+  getStudyDateStringified,
+  studies,
+  studyCrudMock,
+} from '@trackyourhealth/api/testing/util';
 import * as request from 'supertest';
 
 import { ApiStudyFeatureModule } from './api-study-feature.module';
@@ -16,47 +23,215 @@ describe('ApiStudyFeature', () => {
       imports: [ApiStudyFeatureModule, ApiStudyDataModule],
     })
       .overrideProvider(PrismaService)
-      .useValue(prismaStudyMock.service)
+      .useValue(studyCrudMock.service)
       .compile();
 
     app = module.createNestApplication();
     await app.init();
   });
 
-  it('/GET studies', async () => {
-    const expectedStudies = prismaStudyMock.getActiveStudies();
-    expect.assertions(2 + expectedStudies.length);
-    const response = await request(app.getHttpServer()).get(baseRoute);
-    expect(response.status).toStrictEqual(HttpStatus.OK);
-    const studies = response.body;
-    expect(studies.length).toStrictEqual(expectedStudies.length);
-    for (let i = 0; i < expectedStudies.length; i++) {
-      expect(expectedStudies[i].id).toStrictEqual(studies[i].id);
-    }
+  beforeEach(() => {
+    studyCrudMock.clearMocks();
   });
 
-  it('/GET study', async () => {
-    expect.assertions(2);
-    const expectedStudy = prismaStudyMock.getFirstStudy();
-    const address = `${baseRoute}/${expectedStudy.id}`;
-    const response = await request(app.getHttpServer()).get(address);
-    expect(response.status).toStrictEqual(HttpStatus.OK);
-    const study = response.body;
-    expect(study.id).toStrictEqual(expectedStudy.id);
+  describe('GET /studies', () => {
+    it('returns all active studies', async () => {
+      studyCrudMock.findMany.mockResolvedValueOnce(studies);
+      expect.assertions(4 + studies.length);
+      const response = await request(app.getHttpServer()).get(baseRoute);
+      expect(response.status).toStrictEqual(HttpStatus.OK);
+      const actualStudies = response.body;
+      expect(actualStudies.length).toStrictEqual(studies.length);
+      for (let i = 0; i < studies.length; i++) {
+        const expectedStudy = getStudyDateStringified(studies[i]);
+        expect(actualStudies[i]).toStrictEqual(expectedStudy);
+      }
+      expect(studyCrudMock.findMany).toBeCalledTimes(1);
+      expect(studyCrudMock.findMany).toBeCalledWith({
+        ...defaultQueryValues,
+        where: {
+          isActive: true,
+        },
+      });
+    });
+
+    it('returns Internal Server Error on missing database connection', async () => {
+      studyCrudMock.findMany.mockRejectedValueOnce(dbConnectionError);
+      expect.assertions(3);
+      const response = await request(app.getHttpServer()).get(baseRoute);
+      expect(response.status).toStrictEqual(HttpStatus.INTERNAL_SERVER_ERROR);
+      expect(studyCrudMock.findMany).toBeCalledTimes(1);
+      expect(studyCrudMock.findMany).toBeCalledWith({
+        ...defaultQueryValues,
+        where: {
+          isActive: true,
+        },
+      });
+    });
   });
 
-  it('/GET study invalid UUID', async () => {
-    expect.assertions(1);
-    const address = `${baseRoute}/invalidStudyIdThatShouldNeverBeUsed`;
-    const response = await request(app.getHttpServer()).get(address);
-    expect(response.status).toStrictEqual(HttpStatus.BAD_REQUEST);
+  describe('GET /studies/id', () => {
+    it('returns study on valid id', async () => {
+      const study = studies[1];
+      studyCrudMock.findUnique.mockResolvedValueOnce(study);
+      expect.assertions(4);
+      const address = `${baseRoute}/${study.id}`;
+      const response = await request(app.getHttpServer()).get(address);
+      expect(response.status).toStrictEqual(HttpStatus.OK);
+      const expectedStudy = getStudyDateStringified(study);
+      const actualStudy = response.body;
+      expect(actualStudy).toStrictEqual(expectedStudy);
+      expect(studyCrudMock.findUnique).toBeCalledTimes(1);
+      expect(studyCrudMock.findUnique).toBeCalledWith({
+        where: { id: expectedStudy.id },
+      });
+    });
+
+    it('returns Bad Request on invalid UUID', async () => {
+      expect.assertions(1);
+      const address = `${baseRoute}/invalidStudyIdThatShouldNeverBeUsed`;
+      const response = await request(app.getHttpServer()).get(address);
+      expect(response.status).toStrictEqual(HttpStatus.BAD_REQUEST);
+    });
+
+    it('returns Not Found on unused UUID', async () => {
+      studyCrudMock.findUnique.mockRejectedValueOnce(dbKnownError);
+      expect.assertions(3);
+      const unusedId = 'b1f05ae3-4947-4c7d-9e6c-edc74e90cdfa';
+      const address = `${baseRoute}/${unusedId}`;
+      const response = await request(app.getHttpServer()).get(address);
+      expect(response.status).toStrictEqual(HttpStatus.NOT_FOUND);
+      expect(studyCrudMock.findUnique).toBeCalledTimes(1);
+      expect(studyCrudMock.findUnique).toBeCalledWith({
+        where: { id: unusedId },
+      });
+    });
   });
 
-  it('/GET study unused UUID', async () => {
-    expect.assertions(1);
-    const address = `${baseRoute}/b1f05ae3-4947-4c7d-9e6c-edc74e90cdfa`;
-    const response = await request(app.getHttpServer()).get(address);
-    expect(response.status).toStrictEqual(HttpStatus.NOT_FOUND);
+  describe('POST /studies', () => {
+    it('returns Not Found on already used UUID', async () => {
+      studyCrudMock.create.mockRejectedValue(dbKnownError);
+      expect.assertions(3);
+      const unusedId = 'b1f05ae3-4947-4c7d-9e6c-edc74e90cdfa';
+      const response = await request(app.getHttpServer())
+        .post(baseRoute)
+        .send({ study: { id: unusedId } });
+      expect(response.status).toStrictEqual(HttpStatus.NOT_FOUND);
+      expect(studyCrudMock.create).toBeCalledTimes(1);
+      expect(studyCrudMock.create).toBeCalledWith({
+        data: {
+          id: unusedId,
+        },
+      });
+    });
+
+    it('returns study on valid input', async () => {
+      const study = studies[1];
+      studyCrudMock.create.mockResolvedValueOnce(study);
+      expect.assertions(4);
+      const response = await request(app.getHttpServer())
+        .post(baseRoute)
+        .send({ study: study });
+      const expectedStudy = getStudyDateStringified(study);
+      expect(response.status).toStrictEqual(HttpStatus.CREATED);
+      expect(response.body).toStrictEqual(expectedStudy);
+      expect(studyCrudMock.create).toBeCalledTimes(1);
+      expect(studyCrudMock.create).toBeCalledWith({ data: expectedStudy });
+    });
+  });
+
+  describe('POST /studies/id', () => {
+    it('returns Bad Request on invalid UUID', async () => {
+      expect.assertions(1);
+      const address = `${baseRoute}/invalidStudyIdThatShouldNeverBeUsed`;
+      const response = await request(app.getHttpServer()).post(address);
+      expect(response.status).toStrictEqual(HttpStatus.BAD_REQUEST);
+    });
+
+    it('returns Not Found on already used UUID', async () => {
+      studyCrudMock.update.mockRejectedValue(dbKnownError);
+      expect.assertions(3);
+      const unusedId = 'b1f05ae3-4947-4c7d-9e6c-edc74e90cdfa';
+      const address = `${baseRoute}/${unusedId}`;
+      const name = 'some name';
+      const response = await request(app.getHttpServer())
+        .post(address)
+        .send({ study: { name: name } });
+      expect(response.status).toStrictEqual(HttpStatus.NOT_FOUND);
+      expect(studyCrudMock.update).toBeCalledTimes(1);
+      expect(studyCrudMock.update).toBeCalledWith({
+        data: {
+          name: name,
+        },
+        where: {
+          id: unusedId,
+        },
+      });
+    });
+
+    it('returns study on valid input', async () => {
+      const name = 'some name';
+      const study = { ...studies[1], name: name };
+      studyCrudMock.update.mockResolvedValueOnce(study);
+      expect.assertions(4);
+      const address = `${baseRoute}/${study.id}`;
+      const response = await request(app.getHttpServer())
+        .post(address)
+        .send({ study: { name: name } });
+      const expectedStudy = getStudyDateStringified(study);
+      expect(response.status).toStrictEqual(HttpStatus.CREATED);
+      expect(response.body).toStrictEqual(expectedStudy);
+      expect(studyCrudMock.update).toBeCalledTimes(1);
+      expect(studyCrudMock.update).toBeCalledWith({
+        data: {
+          name: name,
+        },
+        where: {
+          id: expectedStudy.id,
+        },
+      });
+    });
+  });
+
+  describe('DELETE /studies/id', () => {
+    it('returns Bad Request on invalid UUID', async () => {
+      expect.assertions(1);
+      const address = `${baseRoute}/invalidStudyIdThatShouldNeverBeUsed`;
+      const response = await request(app.getHttpServer()).delete(address);
+      expect(response.status).toStrictEqual(HttpStatus.BAD_REQUEST);
+    });
+
+    it('returns Not Found on unused UUID', async () => {
+      studyCrudMock.delete.mockRejectedValue(dbKnownError);
+      expect.assertions(3);
+      const unusedId = 'b1f05ae3-4947-4c7d-9e6c-edc74e90cdfa';
+      const address = `${baseRoute}/${unusedId}`;
+      const response = await request(app.getHttpServer()).delete(address);
+      expect(response.status).toStrictEqual(HttpStatus.NOT_FOUND);
+      expect(studyCrudMock.delete).toBeCalledTimes(1);
+      expect(studyCrudMock.delete).toBeCalledWith({
+        where: {
+          id: unusedId,
+        },
+      });
+    });
+
+    it('returns study on valid input', async () => {
+      const study = studies[1];
+      studyCrudMock.delete.mockResolvedValueOnce(study);
+      expect.assertions(4);
+      const address = `${baseRoute}/${study.id}`;
+      const response = await request(app.getHttpServer()).delete(address);
+      const expectedStudy = getStudyDateStringified(study);
+      expect(response.status).toStrictEqual(HttpStatus.OK);
+      expect(response.body).toStrictEqual(expectedStudy);
+      expect(studyCrudMock.delete).toBeCalledTimes(1);
+      expect(studyCrudMock.delete).toBeCalledWith({
+        where: {
+          id: expectedStudy.id,
+        },
+      });
+    });
   });
 
   afterAll(async () => {
---
+++ libs/api/testing/util/src/lib/util/study.prisma.util.ts
@@ -0,0 +1,42 @@
+import { Study } from '@prisma/client';
+
+export const studies: Study[] = [
+  {
+    id: 'eabb4ea7-0ff9-47fc-bb5c-2d11d5273516',
+    name: 'teststudy4321',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    title: 'teststudy',
+    description: 'study for testing',
+    isActive: true,
+    startsAt: new Date(),
+    endsAt: new Date(),
+  },
+  {
+    id: '89606099-b544-4c7c-a172-86951932a803',
+    name: 'teststudy1234',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    title: 'teststudy',
+    description: 'study for testing',
+    isActive: true,
+    startsAt: new Date(),
+    endsAt: new Date(),
+  },
+];
+
+export const getStudyDateStringified = (study: Study) => {
+  return {
+    ...study,
+    createdAt: study.createdAt.toJSON(),
+    updatedAt: study.updatedAt.toJSON(),
+    startsAt: study.startsAt.toJSON(),
+    endsAt: study.endsAt?.toJSON(),
+  };
+};
+
+export const defaultQueryValues = {
+  orderBy: [{ id: 'asc' }],
+  skip: 0,
+  take: 20,
+};
