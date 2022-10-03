39ebdd8d42ab8dd34e35e0243902eab69711bc74
patricksteiger <patricksteiger7@gmail.com>
2022-10-03 16:52:31 +0200

    chore(app): refactor global validation pipe

---
+++ libs/api/common/util/src/lib/pipes/validation.pipe.ts
@@ -0,0 +1,25 @@
+import {
+  HttpStatus,
+  UnprocessableEntityException,
+  ValidationPipe,
+} from '@nestjs/common';
+
+export function createValidationPipe() {
+  return new ValidationPipe({
+    whitelist: true,
+    transform: true,
+    skipMissingProperties: false,
+    skipUndefinedProperties: false,
+    forbidNonWhitelisted: true,
+    errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+    // disable the use of @nestjs packages here for now, otherwise mapped-types does not work properly
+    // validatorPackage: require('@nestjs/class-validator'),
+    // transformerPackage: require('@nestjs/class-transformer'),
+    exceptionFactory: (errors) =>
+      new UnprocessableEntityException({
+        title: 'Validation Exception',
+        message: 'Validation failed',
+        error: errors,
+      }),
+  });
+}
