3fd776e27c0a3d48fced77f43bcced9b33f8f10f
johannesschobel <johannes.schobel@googlemail.com>
2022-10-14 10:22:26 +0200

    chore: generate prisma

--- libs/api/study/data/src/lib/data/inputs/study.input.ts
+++ libs/api/study/data/src/lib/data/inputs/study.input.ts
@@ -8,6 +8,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { OmitType } from '@nestjs/swagger';
 import { PartialType } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
+import { IsDate } from 'class-validator';
 import { IsObject } from 'class-validator';
 import { IsBoolean } from 'class-validator';
 import { IsISO8601 } from 'class-validator';
@@ -15,36 +16,45 @@ import { IsOptional } from 'class-validator';
 
 export class StudyInput {
   @ApiProperty()
+  @IsString()
   id!: string;
 
   @ApiProperty()
+  @IsDate()
   createdAt!: Date;
 
   @ApiProperty()
+  @IsDate()
   updatedAt!: Date;
 
   @ApiProperty()
+  @IsString()
   @IsString()
   name!: string;
 
   @ApiProperty()
   @IsObject()
+  @IsObject()
   title!: object;
 
   @ApiProperty()
+  @IsObject()
   @IsObject()
   description!: object;
 
   @ApiProperty()
   @IsBoolean()
+  @IsBoolean()
   isActive!: boolean;
 
   @ApiProperty()
+  @IsDate()
   @IsISO8601()
   startsAt!: Date;
 
   @ApiProperty({ required: false })
   @IsOptional()
+  @IsDate()
   @IsISO8601()
   endsAt?: Date;
 }
