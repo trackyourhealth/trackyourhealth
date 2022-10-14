d3a26ee34d95c444eb5d00b9948d104ba72caea1
johannesschobel <johannes.schobel@googlemail.com>
2022-10-14 13:40:32 +0200

    refactor: minor

--- libs/api/common/util/src/lib/interceptors/data-transformer.interceptor.ts
+++ libs/api/common/util/src/lib/interceptors/data-transformer.interceptor.ts
@@ -19,6 +19,6 @@ export class DataTransformerInterceptor<T>
     context: ExecutionContext,
     next: CallHandler,
   ): Observable<Response<T>> {
-    return next.handle().pipe(map((data) => ({ data })));
+    return next.handle().pipe(map((data) => ({ data: data })));
   }
 }
