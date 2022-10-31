69f0d6ded2990d5af549338ea5874aa7781d6561
patricksteiger <patricksteiger7@gmail.com>
2022-10-31 15:17:50 +0100

    chore(api): fix instrument errors after rebase

--- libs/api/instrument/data/src/lib/data/inputs/instrument.input.ts
+++ libs/api/instrument/data/src/lib/data/inputs/instrument.input.ts
@@ -7,30 +7,41 @@ THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
 import { ApiProperty } from '@nestjs/swagger';
 import { OmitType } from '@nestjs/swagger';
 import { PartialType } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+import { IsISO8601 } from 'class-validator';
+import { IsObject } from 'class-validator';
 
 export class InstrumentInput {
   @ApiProperty()
+  @IsString()
   id!: string;
 
   @ApiProperty()
+  @IsISO8601()
   createdAt!: Date;
 
   @ApiProperty()
+  @IsISO8601()
   updatedAt!: Date;
 
   @ApiProperty()
+  @IsString()
   name!: string;
 
   @ApiProperty()
+  @IsObject()
   title!: object;
 
   @ApiProperty()
+  @IsObject()
   description!: object;
 
   @ApiProperty()
+  @IsObject()
   questionnaire!: object;
 
   @ApiProperty()
+  @IsObject()
   evaluator!: object;
 }
 
--- libs/api/instrument/data/src/lib/services/instrument.crud.service.ts
+++ libs/api/instrument/data/src/lib/services/instrument.crud.service.ts
@@ -14,6 +14,7 @@ import {
   PaginationInterface,
   PrismaService,
 } from '@prisma-utils/nestjs-prisma';
+import { err, ok, Result } from 'neverthrow';
 
 @Injectable()
 export class InstrumentCrudService {
@@ -25,44 +26,56 @@ export class InstrumentCrudService {
 
   async getAll(
     filter?: Prisma.InstrumentFindManyArgs,
-  ): Promise<PaginationInterface<Instrument>> {
-    const [items, count] = await this.prismaService.$transaction([
-      this.prismaService.instrument.findMany(filter),
-      this.prismaService.instrument.count({ where: filter?.where }),
-    ]);
+  ): Promise<Result<PaginationInterface<Instrument>, Error>> {
+    try {
+      const [items, count] = await this.prismaService.$transaction([
+        this.prismaService.instrument.findMany(filter),
+        this.prismaService.instrument.count({ where: filter?.where }),
+      ]);
 
-    const take = filter?.take ? filter?.take : count;
-    const skip = filter?.skip ? filter?.skip : 0;
+      const take = filter?.take ? filter?.take : count;
+      const skip = filter?.skip ? filter?.skip : 0;
 
-    return {
-      items: items,
-      meta: {
-        totalItems: count,
-        items: items.length,
-        totalPages: Math.ceil(count / take),
-        page: skip / take + 1,
-      },
-    };
+      return ok({
+        items: items,
+        meta: {
+          totalItems: count,
+          items: items.length,
+          totalPages: Math.ceil(count / take),
+          page: skip / take + 1,
+        },
+      });
+    } catch (e) {
+      return err(
+        new InternalServerErrorException(`Could not get Instrument Resources.`),
+      );
+    }
   }
 
-  async getById(id: string): Promise<Instrument> {
+  async getById(id: string): Promise<Result<Instrument, Error>> {
     try {
       const result = await this.prismaService.instrument.findUniqueOrThrow({
         where: { id: id },
       });
-      return result;
+      return ok(result);
     } catch (e) {
-      throw new NotFoundException(`Instrument Resource ${id} was not found.`);
+      return err(
+        new NotFoundException(`Instrument Resource ${id} was not found.`),
+      );
     }
   }
 
-  async create(data: Prisma.InstrumentCreateInput): Promise<Instrument> {
+  async create(
+    data: Prisma.InstrumentCreateInput,
+  ): Promise<Result<Instrument, Error>> {
     try {
       const result = await this.prismaService.instrument.create({ data: data });
-      return result;
+      return ok(result);
     } catch (e) {
-      throw new InternalServerErrorException(
-        `Could not create Instrument Resource`,
+      return err(
+        new InternalServerErrorException(
+          `Could not create Instrument Resource.`,
+        ),
       );
     }
   }
@@ -70,25 +83,33 @@ export class InstrumentCrudService {
   async update(
     id: string,
     data: Prisma.InstrumentUpdateInput,
-  ): Promise<Instrument> {
+  ): Promise<Result<Instrument, Error>> {
     try {
-      return await this.prismaService.instrument.update({
+      const result = await this.prismaService.instrument.update({
         where: { id: id },
         data: data,
       });
+      return ok(result);
     } catch (e) {
-      throw new InternalServerErrorException(
-        `Could not update Instrument Resource ${id}`,
+      return err(
+        new InternalServerErrorException(
+          `Could not update Instrument Resource ${id}.`,
+        ),
       );
     }
   }
 
-  async delete(id: string): Promise<Instrument> {
+  async delete(id: string): Promise<Result<Instrument, Error>> {
     try {
-      return await this.prismaService.instrument.delete({ where: { id: id } });
+      const result = await this.prismaService.instrument.delete({
+        where: { id: id },
+      });
+      return ok(result);
     } catch (e) {
-      throw new InternalServerErrorException(
-        `Could not delete Instrument Model ${id}`,
+      return err(
+        new InternalServerErrorException(
+          `Could not delete Instrument Resource ${id}.`,
+        ),
       );
     }
   }
